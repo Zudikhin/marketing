392c0b3c98b4064e9ab041e5ca99b655bbff955f
Zudikhin <uzudihin@gmail.com>
2024-07-22 00:00:25 +0300

    all

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+  "use strict";
+
+  if($(window).width() > 1199) {
+    $('.what_slider_wrap').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      prevArrow: $('.what_slider_prev'),
+      nextArrow: $('.what_slider_next')
+    });
+  }
+
+  if($(window).width() > 1199) {
+    $('.services_slider').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      prevArrow: $('.services_title_btns_prev'),
+      nextArrow: $('.services_title_btns_next')
+    });
+  }
+
+  if($(window).width() > 1199) {
+    $('.result_slider_wrap').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      prevArrow: $('.result_slider_prev'),
+      nextArrow: $('.result_slider_next')
+    });
+  }
+
+  $('.marketing_slider').slick({
+    slidesToScroll: 1,
+    variableWidth: true,
+    prevArrow: $('.marketing_prev'),
+    nextArrow: $('.marketing_next'),
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          variableWidth: false,
+          fade: false
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          variableWidth: false,
+          fade: true
+        }
+      }
+    ]
+  });
+    
+});
